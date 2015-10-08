a0176569d7d43f3fc5c25e3a84604dd9f7f2f17b
Thomas Heaton <thomasaheaton@gmail.com>
2015-10-07 23:56:52 -0400

    header complete

--- js/main.js
+++ js/main.js
@@ -1,5 +1,19 @@
 (function () {
 
-  console.log('It Works!');
+  //var firstItem = etsy.results[0];
+
+  var templateString = $('#itemTemplate').text();
+
+  var templateFunction = _.template(templateString);
+
+  console.log(etsy.results);
+
+  _.each(etsy.results, function (item) {
+    console.log(item);
+  var itemHTML = templateFunction(item);
+  // $('.container').append(itemHTML)
+
+  $('.container').append(itemHTML);
+  });
 
 }());
