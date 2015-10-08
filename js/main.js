ea8387b7c397ae958ab27119bfb6022db03f4628
Thomas Heaton <thomasaheaton@gmail.com>
2015-10-08 15:26:46 -0400

    wip - menus and pics

--- js/main.js
+++ js/main.js
@@ -1,16 +1,20 @@
 (function () {
 
-  //var firstItem = etsy.results[0];
+  var firstItem = etsy.results[0];
 
   var templateString = $('#itemTemplate').text();
 
   var templateFunction = _.template(templateString);
-
+  //mine
+  
+  //end mine
   console.log(etsy.results);
 
   _.each(etsy.results, function (item) {
     console.log(item);
   var itemHTML = templateFunction(item);
+  
+
   // $('.container').append(itemHTML)
 
   $('.container').append(itemHTML);
